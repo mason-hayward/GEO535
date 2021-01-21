df84e92d7ca5b62c3ce927967e253b90306acec1
Mason Hayward <mhayward1994@gmail.com>
2021-01-21 09:10:44 -0600

    Altered factorial to remove recursiveness

--- GEO535_prg3_factorialfunction.py
+++ GEO535_prg3_factorialfunction.py
@@ -5,11 +5,16 @@ Created on Tue Jan 19 14:58:17 2021
 @author: C00452874
 """
 def factorial(n):
-    if n == 1:
-        factValue = 1
-    if n != 1:
-        factValue = n*factorial(n-1)
+    factValue = 1
+    for i in range(1, n+1):
+        factValue *= i
     return factValue
+#Recursive Formula (Fails at n=2961)
+#    if n == 1:
+#        factValue = 1
+#    if n != 1:
+#        factValue = n*factorial(n-1)
+#    return factValue
     
 n = int(input("Input Value: "))
 print(factorial(n))
